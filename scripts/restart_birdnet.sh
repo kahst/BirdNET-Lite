7a0280654d3e3c8d3d45dad0f09edd199a14de67
Patrick McGuire <mcguirepr89@gmail.com>
2021-10-02 18:58:26 -0400

    adding new script for birdnet.conf check

---
+++ scripts/restart_birdnet.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Restart services
+source /etc/birdnet/birdnet.conf
+
+sudo systemctl stop birdnet_recording.service
+rm -rf ${RECS_DIR}/$(date +%B-%Y/%d-%A)/*
+sudo systemctl start birdnet_recording.service
+sudo systemctl restart extraction.timer
+sudo systemctl restart birdnet_analysis.service
