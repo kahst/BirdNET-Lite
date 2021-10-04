47a5c64430562e896f34df65677dc3d63923ced8
Patrick McGuire <mcguirepr89@gmail.com>
2021-10-04 12:21:36 -0400

    added "Clear all data" button and "Shutdown BirdNET-Lite" button

---
+++ scripts/clear_all_data.php
@@ -0,0 +1,4 @@
+<?php
+shell_exec("/home/pi/BirdNET-Lite/scripts/clear_all_data.sh");
+header('Location: http://birdnetsystem.local/scripts/index.html?success=true');
+?>
--- scripts/clear_all_data.sh
+++ scripts/clear_all_data.sh
@@ -4,38 +4,19 @@
 # you are okay will losing all the data that you've collected and processed
 # so far.
 source /etc/birdnet/birdnet.conf
-
-echo "
-This script removes all data that has been collected. It is tantamount to
-starting all data-collection from scratch. Only run this if you are sure
-you are okay with losing all the data that you've collected and processed
-so far.
-
-"
-read -n1 -p "Are you sure you want to wipe away ALL data?" YN
-echo
-while true; do
-  case $YN in
-    [Yy]) break;;
-    *) echo "Exiting since you didn't answer with Y or y." && exit;;
-  esac
-done
-
 echo "Stopping services"
 sudo systemctl stop birdnet_recording.service
 echo "Removing all data . . . "
 sudo rm -drf "${RECS_DIR}"
 rm "${IDFILE}"
-
 echo "Recreating necessary directories"
-[ -d ${RECS_DIR} ] || mkdir -p ${RECS_DIR}
-[ -d ${EXTRACTED} ] || mkdir -p ${EXTRACTED}
-[ -d ${EXTRACTED}/By_Date ] || mkdir -p ${EXTRACTED}/By_Date
-[ -d ${EXTRACTED}/By_Common_Name ] || mkdir -p ${EXTRACTED}/By_Common_Name
-[ -d ${EXTRACTED}/By_Scientific_Name ] || mkdir -p ${EXTRACTED}/By_Scientific_Name
-[ -d ${PROCESSED} ] || mkdir -p ${PROCESSED}
-cp ~/BirdNET-Lite/templates/index.html ${EXTRACTED}/
-
-
+[ -d ${RECS_DIR} ] || sudo -u ${BIRDNET_USER} mkdir -p ${RECS_DIR}
+[ -d ${EXTRACTED} ] || sudo -u ${BIRDNET_USER} mkdir -p ${EXTRACTED}
+[ -d ${EXTRACTED}/By_Date ] || sudo -u ${BIRDNET_USER} mkdir -p ${EXTRACTED}/By_Date
+[ -d ${EXTRACTED}/By_Common_Name ] || sudo -u ${BIRDNET_USER} mkdir -p ${EXTRACTED}/By_Common_Name
+[ -d ${EXTRACTED}/By_Scientific_Name ] || sudo -u ${BIRDNET_USER} mkdir -p ${EXTRACTED}/By_Scientific_Name
+[ -d ${PROCESSED} ] || sudo -u ${BIRDNET_USER} mkdir -p ${PROCESSED}
+[ -L ${EXTRACTED}/scripts ] || sudo -u ${BIRDNET_USER} ln -s /home/pi/BirdNET-Lite/scripts ${EXTRACTED}/
+sudo -u ${BIRDNET_USER} cp ~/BirdNET-Lite/templates/index.html ${EXTRACTED}/
 echo "Restarting services"
 sudo systemctl restart birdnet_recording.service
