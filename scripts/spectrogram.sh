6fb9a50e0fca9e822d7d39d25c17d576fdf036f4
Patrick McGuire <mcguirepr89@gmail.com>
2021-10-08 14:50:20 -0400

    better spectrograms

--- scripts/spectrogram.sh
+++ scripts/spectrogram.sh
@@ -3,4 +3,4 @@
 source /etc/birdnet/birdnet.conf
 analyzing_now="$(cat /home/pi/Birding-Pi/analyzing_now.txt)"
 spectrogram_png=${EXTRACTED}/spectrogram.png
-sudo -u pi sox "${analyzing_now}" -n spectrogram -o "${spectrogram_png}"
+sudo -u pi sox "${analyzing_now}" -n spectrogram -t "Currently Analyzing" -c "${analyzing_now}" -o "${spectrogram_png}"
