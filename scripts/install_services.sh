8ea70541766cad1f7116d62f86b135bd9f49b71a
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2021-10-15 15:57:52 -0400

    Update install_services.sh

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -218,7 +218,7 @@ ${EXTRACTIONS_URL} {
   basicauth /stream {
     birdnet ${HASHWORD}
   }
-  basicauth /phpsysinfo {
+  basicauth /phpsysinfo* {
     birdnet ${HASHWORD}
   }  
   reverse_proxy /stream localhost:8000
@@ -237,7 +237,7 @@ http://birdnetpi.local {
   basicauth /stream {
     birdnet ${HASHWORD}
   }
-  basicauth /phpsysinfo {
+  basicauth /phpsysinfo* {
     birdnet ${HASHWORD}
   } 
   reverse_proxy /stream localhost:8000
