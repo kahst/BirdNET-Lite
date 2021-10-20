064f5f61535d92cd84619c898053312b151d7473
Patrick McGuire <mcguirepr89@gmail.com>
2021-10-20 11:45:06 -0400

    adding step to update_birdnet2.sh

--- scripts/update_birdnet2.sh
+++ scripts/update_birdnet2.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 # Second stage of update
 
+# Stage 1 updates the birdnet.conf
+sudo -upi sed -i 's/EXTRACTIONS_URL/BIRDNETPI_URL/g' /home/pi/BirdNET-Pi/birdnet.conf
+echo "EXTRACTIONLOG_URL=" >> /home/pi/BirdNET-Pi/birdnet.conf
+echo "BIRDNETLOG_URL=" >> /home/pi/BirdNET-Pi/birdnet.conf
 
-# Stage 1 updates the services
+# Stage 2 updates the services
 my_dir=${HOME}/BirdNET-Pi/scripts
 sudo ${my_dir}/update_services.sh
 
