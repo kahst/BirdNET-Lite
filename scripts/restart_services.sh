1603ae7a5f184f92e2af37a6d5fb903ac2806a51
Patrick McGuire <mcguirepr89@gmail.com>
2021-10-22 20:39:02 +0200

    fixing Dennis bug

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Restarts ALL services and removes ALL unprocessed audio
 source /etc/birdnet/birdnet.conf
+my_dir=/home/pi/BirdNET-Pi/scripts
 
 sudo systemctl stop birdnet_recording.service
 sudo rm -rf ${RECS_DIR}/$(date +%B-%Y/%d-%A)/*
@@ -9,6 +10,7 @@ sudo systemctl start birdnet_recording.service
 services=$(awk '/service/ && /systemctl/ && !/php/ {print $3}' ${my_dir}/install_services.sh | sort)
 
 for i in  "${services[@]}";do
-  sudo systemctl restart ${i}
+	echo $i
+#  sudo systemctl restart ${i}
 done
 
