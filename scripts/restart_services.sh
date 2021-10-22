3770e3f52ce3aff4fba0f7b631c289cb01d4ee59
Patrick McGuire <mcguirepr89@gmail.com>
2021-10-22 20:58:47 +0200

    fixed

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -7,9 +7,9 @@ sudo systemctl stop birdnet_recording.service
 sudo rm -rf ${RECS_DIR}/$(date +%B-%Y/%d-%A)/*
 sudo systemctl start birdnet_recording.service
 
-services=$(awk '/service/ && /systemctl/ && !/php/ {print $3}' ${my_dir}/install_services.sh | sort)
+services=($(awk '/service/ && /systemctl/ && !/php/ {print $3}' ${my_dir}/install_services.sh | sort))
 
 for i in  "${services[@]}";do
-sudo systemctl restart ${i}
+sudo systemctl restart "${i}"
 done
 
