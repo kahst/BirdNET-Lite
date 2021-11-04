67e286f60dcdb32c08e341bc84ece17c695bed03
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2021-11-04 09:35:35 -0400

    Update birdnet_analysis.sh

--- scripts/birdnet_analysis.sh
+++ scripts/birdnet_analysis.sh
@@ -7,7 +7,7 @@ source /etc/birdnet/birdnet.conf
 # the birdnet.conf as it was the last time this script was called
 my_dir=$(realpath $(dirname $0))
 if [ -z ${THIS_RUN} ];then THIS_RUN=/home/pi/BirdNET-Pi/thisrun.txt;fi
-if [ -z ${LAST_RUN} ];then LAST_RUN=/home/pi/BirdNET-Pi/thisrun.txt;fi
+if [ -z ${LAST_RUN} ];then LAST_RUN=/home/pi/BirdNET-Pi/lastrun.txt;fi
 if [ -z ${LATITUDE} ];then LATITUDE=-1;fi
 if [ -z ${LONGITUDE} ];then LONGITUDE=-1;fi
 make_thisrun() {
