97c0e394b3dbcfc29c9ceba1304989d0dfade87f
Patrick McGuire <mcguirepr89@gmail.com>
2021-12-01 15:39:55 -0500

    birdweather updates and webpage aesthetic changes

--- scripts/update_birdnet.sh
+++ scripts/update_birdnet.sh
@@ -52,7 +52,7 @@ remove_services
 remove_scripts
 
 # Stage 2 does a git pull to fetch new things
-sudo -u${USER} git -C ${HOME}/BirdNET-Pi checkout -f
-sudo -u${USER} git -C ${HOME}/BirdNET-Pi pull -f
+#sudo -u${USER} git -C ${HOME}/BirdNET-Pi checkout -f
+#sudo -u${USER} git -C ${HOME}/BirdNET-Pi pull -f
 # Trigger the new update_birdnet2.sh
 sudo -u${USER} ${my_dir}/update_birdnet2.sh
