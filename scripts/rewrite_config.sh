d3e1b0cb36c5d99348c0ad601ead5b9a5ac26866
mcguirepr89 <mcguirepr89@gmail.com>
2022-01-15 16:29:52 -0500

    added BirdWeather ID support

--- scripts/rewrite_config.sh
+++ scripts/rewrite_config.sh
@@ -24,6 +24,10 @@ sed -i s/'^DB_PWD=.*'/"DB_PWD=${db_pwd}"/g ${birdnet_conf}
 ${birdnetpi_dir}/scripts/update_db_pwd_bullseye.sh
 fi
 
+if ! [ -z ${birdweather_id} ];then
+sed -i s/'^BIRDWEATHER_ID=.*'/"BIRDWEATHER_ID=${birdweather_id}"/g ${birdnet_conf}
+fi
+
 if ! [ -z ${birdnetpi_url} ];then
 sed -i s/'^BIRDNETPI_URL=.*'/"BIRDNETPI_URL=${birdnetpi_url/\/\//\\\/\\\/}"/g ${birdnet_conf}
 sed -i s/'^EXTRACTIONLOG_URL=.*'/"EXTRACTIONLOG_URL=${extractionlog_url/\/\//\\\/\\\/}"/g ${birdnet_conf}
