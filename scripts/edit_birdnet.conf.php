60fdabe898f814a90aac23ad3ca543011677d98f
mcguirepr89 <mcguirepr89@gmail.com>
2022-01-19 10:55:07 -0500

    deleted old files

--- scripts/edit_birdnet.conf.php
+++
@@ -1,4 +0,0 @@
-<?php
-shell_exec("/home/pi/BirdNET-Pi/scripts/edit_birdnet.conf.sh");
-header('Location: http://birdnetpi.local:9898');
-?>
--- scripts/edit_birdnet.conf.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo systemctl start edit_birdnet_conf.service
