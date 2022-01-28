6d5ea98beee872ab8b2e3c9da15fc538863b4176
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-01-27 20:48:00 -0500

    Update rebuild_db.sh

--- scripts/rebuild_db.sh
+++ scripts/rebuild_db.sh
@@ -2,7 +2,7 @@
 # Rebuild DB from BirdDB.txt
 source /etc/birdnet/birdnet.conf
 BIRDNET_DIR=/home/pi/BirdNET-Pi
-sudo ${BIRDNET_DIR}/scripts/createdb.sh
+sudo ${BIRDNET_DIR}/scripts/createdb_bullseye.sh
 sudo mysql birds -e "
   LOAD DATA LOCAL INFILE '/home/pi/BirdNET-Pi/BirdDB.txt'
   INTO TABLE detections
