8d8f8759b48556e3e9cbceb6c229f39f30fd2e99
mcguirepr89 <mcguirepr89@gmail.com>
2022-02-02 11:58:25 -0500

    spectrogram refreshes using RECORDING_LENGTH variable

--- scripts/spectrogram.php
+++ scripts/spectrogram.php
@@ -1,5 +1,11 @@
 <?php
-header("refresh: 15;");
-echo "<body style='background-color:rgb(119, 196, 135)'>";
+if (file_exists('/home/pi/BirdNET-Pi/thisrun.txt')) {
+  $config = parse_ini_file('/home/pi/BirdNET-Pi/thisrun.txt');
+} elseif (file_exists('/home/pi/BirdNET-Pi/firstrun.ini')) {
+  $config = parse_ini_file('/home/pi/BirdNET-Pi/firstrun.ini');
+}
+$refreshtime = $config['RECORDING_LENGTH'];
+header("refresh:$refreshtime");
 ?>
+<body style='background-color:rgb(119, 196, 135)'>
 <img src='/spectrogram.png?nocache=<?php echo time();?>' style='display: block; height: 100%; width: 100%;'> 
