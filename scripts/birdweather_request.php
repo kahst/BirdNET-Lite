7a4cc4bc7053b91e387cdd842ad08de4b4657902
mcguirepr89 <mcguirepr89@gmail.com>
2022-02-02 08:15:13 -0500

    adding disclaimer to email_template

--- scripts/birdweather_request.php
+++ scripts/birdweather_request.php
@@ -4,7 +4,7 @@ if (file_exists('/home/pi/BirdNET-Pi/thisrun.txt')) {
 } elseif (file_exists('/home/pi/BirdNET-Pi/firstrun.ini')) {
   $config = parse_ini_file('/home/pi/BirdNET-Pi/firstrun.ini');
 } 
-$template = file_get_contents("email_template.html");
+$template = file_get_contents("email_template");
 
 foreach($config as $key => $value)
 {
