1208b4346a596d65c244b20f973010320071f239
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-02-03 15:45:52 -0500

    Update update_inprogress.php

--- scripts/update_inprogress.php
+++ scripts/update_inprogress.php
@@ -1,11 +1,40 @@
 <?php
 $timer=60;
 header( "refresh:$timer;url=/viewdb.php" );
-$handle = popen("tail -f /tmp/phpupdate.log", 'r');
-while(!feof($handle)) {
-    $buffer = fgets($handle);
-    echo "$buffer<br/>\n";
-    ob_flush();
-    flush();
+?>
+<html lang="en">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<head></head>
+	<body style="background-color: rgb(119, 196, 135)">
+<script>
+
+function countDown(secs,elem) {
+
+	var element = document.getElementById(elem);
+
+	element.innerHTML = "Update in progress. Please allow another "+secs+" seconds for it to complete.";
+
+	if(secs < 1) {
+
+		clearTimeout(timer);
+
+		element.innerHTML = '<h4>Let\'s see</h4>';
+
+		element.innerHTML += '<a href="http://birdnetpi.local"></a>';
+
+	}
+
+	secs--;
+
+	var timer = setTimeout('countDown('+secs+',"'+elem+'")',1000);
+
 }
-pclose($handle);
+
+</script>
+
+<div id="status"style="font-size:30px;"></div>
+
+<script>countDown(<?php echo $timer;?>,"status");</script>
+	</body>
+</html>
