806f1b5f4579a7ab109f444474636a2fd4eac16e
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-02-18 15:32:44 -0500

    Update newinstaller.sh

--- newinstaller.sh
+++ newinstaller.sh
@@ -9,8 +9,7 @@ if ! which git &> /dev/null;then
   sudo apt -y install git
 fi
 git clone -b ${branch} https://github.com/mcguirepr89/BirdNET-Pi.git ${HOME}/BirdNET-Pi &&
-set -x
-exec > >(tee -i installation$(date +%F).txt) 2>&1
+
 ${HOME}/BirdNET-Pi/scripts/install_birdnet.sh
 if [ ${PIPESTATUS[0]} -eq 0 ];then
   echo "Installation completed successfully"
