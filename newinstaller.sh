e2acc020f12adbd10b9ebc2e7386c406091cd345
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-02-18 14:13:10 -0500

    Update newinstaller.sh

--- newinstaller.sh
+++ newinstaller.sh
@@ -8,5 +8,4 @@ if ! which git &> /dev/null;then
   sudo apt -y install git
 fi
 git clone -b ${branch} https://github.com/mcguirepr89/BirdNET-Pi.git ${HOME}/BirdNET-Pi &&
-cp ${HOME}/BirdNET-Pi/birdnet.conf-defaults ${HOME}/BirdNET-Pi/birdnet.conf &&
 ${HOME}/BirdNET-Pi/scripts/install_birdnet.sh
