78f502423562ea8b0fc69cef670309545fe13aed
mcguirepr89 <mcguirepr89@gmail.com>
2022-03-08 15:51:15 -0500

    styled exclude species list settings page

--- scripts/exclude_list.php
+++ scripts/exclude_list.php
@@ -2,41 +2,56 @@
 <style>
 </style>
 
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors',1);
-
-$filename = 'scripts/labels.txt';
-$eachline = file($filename, FILE_IGNORE_NEW_LINES);
-
-?>
-
 <body>
-      <h2>All Species Labels</h2>
-      <form action="" method="POST" id="add">
-  <select name="species[]" id="species" multiple size="30">
-  <option selected value="base">Please Select</option>
-<?php   
-foreach($eachline as $lines){
-  echo "<option value='".$lines."'>$lines</option>";
-}?>
-        </select>
-      <input type="hidden" name="add" value="add">
-      </form>
+<div class="customlabels column1">
+<form action="" method="POST" id="add">
+  <h3>All Species Labels</h3>
+  <select name="species[]" id="species" multiple size="auto">
+  <option>Choose a species below to add to the Excluded Species List</option>
+  <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors',1);
+    
+    $filename = 'scripts/labels.txt';
+    $eachline = file($filename, FILE_IGNORE_NEW_LINES);
+    
+    foreach($eachline as $lines){echo 
+  "<option value='".$lines."'>$lines</option>";}
+  ?>
+  </select>
+  <input type="hidden" name="add" value="add">
+</form>
+<div class="customlabels smaller">
+  <button type="submit" name="view" value="Excluded" form="add">>>ADD>></button>
+</div>
+</div>
 
-      <button type="submit" name="view" value="Excluded" form="add">Add to list</button><br>
-      <button type="submit" name="view" value="Excluded" form="del">Remove from list</button>
+<div class="customlabels column2">
+  <table><td>
+  <button type="submit" name="view" value="Excluded" form="add">>>ADD>></button><br>
+  <br>
+  <button type="submit" name="view" value="Excluded" form="del">REMOVE</button>
+  </td></table>
+</div>
 
-        <h2>Excluded Species List</h2>
-        <form action="" method="POST" id="del">
-  <select name="species[]" id="value2" multiple size="30">
-  <option selected value="base">Please Select</option>
+<div class="customlabels column3">
+<form action="" method="POST" id="del">
+  <h3>Excluded Species List</h3>
+  <select name="species[]" id="value2" multiple size="auto">
 <?php
   $filename = '/home/pi/BirdNET-Pi/exclude_species_list.txt';
-$eachline = file($filename, FILE_IGNORE_NEW_LINES);
-foreach($eachline as $lines){
-  echo "<option value='".$lines."'>$lines</option>";
+  $eachline = file($filename, FILE_IGNORE_NEW_LINES);
+  foreach($eachline as $lines){
+    echo 
+  "<option value='".$lines."'>$lines</option>";
 }?>
-      <input type="hidden" name="del" value="del">
-      </form>
+  </select>
+  <input type="hidden" name="del" value="del">
+</form>
+<div class="customlabels smaller">
+  <button type="submit" name="view" value="Excluded" form="del">REMOVE</button>
+</div>
+</div>
+
+
 </body>
