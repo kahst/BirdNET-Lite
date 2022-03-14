7ef4775cbc17c897fa3c3944021b065c867b99b8
mcguirepr89 <mcguirepr89@gmail.com>
2022-03-14 09:53:14 -0400

    fixed bad play.php (didn't dig properly)

--- scripts/play.php
+++ scripts/play.php
@@ -9,16 +9,21 @@ if($db == False){
   header("refresh: 0;");
 }
 
+#By Date
 if(isset($_POST['bydate'])){
-  $statement = $db->prepare('SELECT DISTINCT(Date), Com_Name FROM detections GROUP BY Date');
+  $statement = $db->prepare('SELECT DISTINCT(Date) FROM detections GROUP BY Date');
   if($statement == False){
     echo "Database is busy";
     header("refresh: 0;");
   }
   $result = $statement->execute();
   $view = "bydate";
+
+  #Specific Date
 } elseif(isset($_POST['date'])) {
   $date = $_POST['date'];
+  session_start();
+  $_SESSION['date'] = $date;
   $statement = $db->prepare("SELECT DISTINCT(Com_Name) FROM detections WHERE Date == \"$date\" ORDER BY Com_Name");
   if($statement == False){
     echo "Database is busy";
@@ -26,8 +31,23 @@ if(isset($_POST['bydate'])){
   }
   $result = $statement->execute();
   $view = "date";
+
+  #By Species
+} elseif(isset($_POST['byspecies'])) {
+  $statement = $db->prepare('SELECT DISTINCT(Com_Name) FROM detections ORDER BY Com_Name');
+  session_start();
+  if($statement == False){
+    echo "Database is busy";
+    header("refresh: 0;");
+  }
+  $result = $statement->execute();
+  $view = "byspecies";
+
+  #Specific Species
 } elseif(isset($_POST['species'])) {
   $species = $_POST['species'];
+  session_start();
+  $_SESSION['species'] = $species;
   $statement = $db->prepare("SELECT * FROM detections WHERE Com_Name == \"$species\" ORDER BY Com_Name");
   $statement3 = $db->prepare("SELECT Date, Time, Sci_Name, MAX(Confidence), File_Name FROM detections WHERE Com_Name == \"$species\" ORDER BY Com_Name");
   if($statement == False || $statement3 == False){
@@ -37,17 +57,9 @@ if(isset($_POST['bydate'])){
   $result = $statement->execute();
   $result3 = $statement3->execute();
   $view = "species";
-} elseif(isset($_POST['byspecies'])) {
-  $statement = $db->prepare('SELECT DISTINCT(Com_Name) FROM detections ORDER BY Com_Name');
-  if($statement == False){
-    echo "Database is busy";
-    header("refresh: 0;");
-  }
-  $result = $statement->execute();
-  $view = "byspecies";
 } else {
-  $statement = $db->prepare('SELECT DISTINCT(Date), Com_Name FROM detections GROUP BY Date');
-  $result = $statement->execute();
+  session_start();
+  session_unset();
   $view = "choose";
 }
 
@@ -60,7 +72,9 @@ if(isset($_POST['bydate'])){
     <style>
     </style>
   </head>
+
 <?php
+#If no specific species
 if(!isset($_POST['species'])){
 ?>
 <div class="play">
@@ -69,75 +83,71 @@ if(!isset($_POST['species'])){
     <form action="" method="POST">
     <input type="hidden" name="view" value="Recordings">
 <?php
+  #By Date
   if($view == "bydate") {
     while($results=$result->fetchArray(SQLITE3_ASSOC)){
-    $date = $results['Date'];
-    echo "<td>
-    <button action=\"submit\" name=\"date\" value=\"$date\">$date</button></td></tr>";}
+      $date = $results['Date'];
+      echo "<td>
+        <button action=\"submit\" name=\"date\" value=\"$date\">$date</button></td></tr>";}
+
+  #By Species
   } elseif($view == "byspecies") {
     while($results=$result->fetchArray(SQLITE3_ASSOC)){
-    $name = $results['Com_Name'];
-    echo "<td>
-    <button action=\"submit\" name=\"species\" value=\"$name\">$name</button></td></tr>";}
+      $name = $results['Com_Name'];
+      echo "<td>
+        <button action=\"submit\" name=\"species\" value=\"$name\">$name</button></td></tr>";}
+
+  #Specific Date
   } elseif($view == "date") {
     while($results=$result->fetchArray(SQLITE3_ASSOC)){
-    $name = $results['Com_Name'];
-    echo "<td>
-    <button action=\"submit\" name=\"species\" value=\"$name\">$name</button></td></tr>";}
-  } elseif($view == "choose") {
-    $date = "By Date";
-    $species = "By Species";
-    echo "<td>
-    <button action=\"submit\" name=\"byspecies\" value=\"byspecies\">$species</button></td></tr>
-    <tr><td><button action=\"submit\" name=\"bydate\" value=\"bydate\">$date</button></td>";
-  } else {
-    while($results=$result->fetchArray(SQLITE3_ASSOC)){
-    $maxconf = $results['MAX(Confidence)'];
-    $date = $results['Date'];
-    $time = $results['Time'];
-    $sciname = $results['Sci_Name'];
-    $sci_name = preg_replace('/ /', '_', $sciname);
-    $sci_name = preg_replace('/ /', '_', $sci_name);
-    $comname = preg_replace('/ /', '_', $species);
-    $comname = preg_replace('/\'/', '', $comname);
-    $file = $results['File_Name'];
-    $filename = "/By_Date/".$date."/".$comname."/".$results['File_Name'];
-    echo "<th>$species</th>
-      <td style=\"vertical-align:middle;\"><a href=\"https://wikipedia.org/wiki/$sci_name\" target=\"top\"><i>$sciname</i></a></td>
-      <td class=\"spectrogram\">Best Recording<br>$date $time<br>$maxconf<br><video controls poster=\"$filename.png\" preload=\"none\" title=\"$filename\"><source src=\"$filename\"></video></td>
-      </tr></table>";
-    }}
-     echo "</form>
-  </tr>
-</table>";
-}
-  if(isset($_POST['species'])){
-    $name = $_POST['species'];
-    $statement2 = $db->prepare("SELECT * FROM detections where Com_Name == \"$name\" ORDER BY Date DESC, Time DESC");
-    if($statement2 == False){
-      echo "Database is busy";
-      header("refresh: 0;");
-    }
-    $result2 = $statement2->execute();
-    echo "<table>
-	    <tr>
-		<th>$name</th>
-            </tr>";
-      while($results=$result2->fetchArray(SQLITE3_ASSOC))
-      {
-        $comname = preg_replace('/ /', '_', $results['Com_Name']);
-        $comname = preg_replace('/\'/', '', $comname);
-        $date = $results['Date'];
-        $filename = "/By_Date/".$date."/".$comname."/".$results['File_Name'];
-        $sciname = preg_replace('/ /', '_', $results['Sci_Name']);
-        $sci_name = $results['Sci_Name'];
-        $time = $results['Time'];
-        $confidence = $results['Confidence'];
-        echo "<tr>
-          <td>$date $time<br>$confidence<br>
-          <video controls poster=\"$filename.png\" preload=\"none\" title=\"$filename\"><source src=\"$filename\"></video></td>
-          </tr>";
+      $name = $results['Com_Name'];
+      echo "<td>
+        <button action=\"submit\" name=\"species\" value=\"$name\">$name</button></td></tr>";}
 
-      }echo "</table>";}?>
+  #Choose
+  } else {
+    echo "<td>
+      <button action=\"submit\" name=\"byspecies\" value=\"byspecies\">By Species</button></td></tr>
+      <tr><td><button action=\"submit\" name=\"bydate\" value=\"bydate\">By Date</button></td>";
+  } 
+
+  echo "</form>
+  </tr>
+  </table>";
+}
+
+#Specific Species
+if(isset($_POST['species'])){
+  $name = $_POST['species'];
+  if(isset($_SESSION['date'])) {
+    $date = $_SESSION['date'];
+    $statement2 = $db->prepare("SELECT * FROM detections where Com_Name == \"$name\" AND Date == \"$date\" ORDER BY Time DESC");
+  } else {
+  $statement2 = $db->prepare("SELECT * FROM detections where Com_Name == \"$name\" ORDER BY Date DESC, Time DESC");}
+  if($statement2 == False){
+    echo "Database is busy";
+    header("refresh: 0;");
+  }
+  $result2 = $statement2->execute();
+  echo "<table>
+    <tr>
+    <th>$name</th>
+    </tr>";
+    while($results=$result2->fetchArray(SQLITE3_ASSOC))
+    {
+      $comname = preg_replace('/ /', '_', $results['Com_Name']);
+      $comname = preg_replace('/\'/', '', $comname);
+      $date = $results['Date'];
+      $filename = "/By_Date/".$date."/".$comname."/".$results['File_Name'];
+      $sciname = preg_replace('/ /', '_', $results['Sci_Name']);
+      $sci_name = $results['Sci_Name'];
+      $time = $results['Time'];
+      $confidence = $results['Confidence'];
+      echo "<tr>
+        <td>$date $time<br>$confidence<br>
+        <video controls poster=\"$filename.png\" preload=\"none\" title=\"$filename\"><source src=\"$filename\"></video></td>
+        </tr>";
+
+    }echo "</table>";}?>
 </div>
 </html>
