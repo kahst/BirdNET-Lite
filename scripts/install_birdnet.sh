536c8091e3f9c97c17cee87e1c7d00da56957083
mcguirepr89 <mcguirepr89@gmail.com>
2022-04-11 12:00:20 -0400

    specifying USER and HOME for sudo

--- scripts/install_birdnet.sh
+++ scripts/install_birdnet.sh
@@ -21,7 +21,7 @@ fi
 
 #Install/Configure /etc/birdnet/birdnet.conf
 ./install_config.sh || exit 1
-sudo -E ./install_services.sh || exit 1
+HOME=$HOME USER=$USER sudo -E ./install_services.sh || exit 1
 source /etc/birdnet/birdnet.conf
 
 install_birdnet() {
