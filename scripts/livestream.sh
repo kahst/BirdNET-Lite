ff9f3552712c4fbb0066789eebd7555116a7bb3e
ehpersonal38 <103586016+ehpersonal38@users.noreply.github.com>
2022-04-25 10:24:23 -0400

    Update livestream.sh

--- scripts/livestream.sh
+++ scripts/livestream.sh
@@ -4,8 +4,12 @@ source /etc/birdnet/birdnet.conf
 
 if [ -z ${REC_CARD} ];then
   echo "Stream not supported"
+elif [[ ! -z ${RTSP_STREAM} ]];then
+  ffmpeg -loglevel 52 -ac ${CHANNELS} -i ${RTSP_STREAM} -acodec libmp3lame \
+    -b:a 320k -ac ${CHANNELS} -content_type 'audio/mpeg' \
+    -f mp3 icecast://source:${ICE_PWD}@localhost:8000/stream -re
 else
-  ffmpeg -loglevel 52 -ac ${CHANNELS} -f alsa -i ${REC_CARD} -acodec libmp3lame \
+	ffmpeg -loglevel 52 -ac ${CHANNELS} -f alsa -i ${REC_CARD} -acodec libmp3lame \
     -b:a 320k -ac ${CHANNELS} -content_type 'audio/mpeg' \
     -f mp3 icecast://source:${ICE_PWD}@localhost:8000/stream -re
 fi
