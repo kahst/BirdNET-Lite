50f0ed86162f27b6e41ba4d43a08a1c9a7978c01
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-05-05 10:37:17 -0400

    Update birdnet_recording.sh

--- scripts/birdnet_recording.sh
+++ scripts/birdnet_recording.sh
@@ -7,7 +7,7 @@ source /etc/birdnet/birdnet.conf
 if [ ! -z $RTSP_STREAM ];then
   while true;do
     for i in ${RTSP_STREAM//,/ };do
-      ffmpeg -i  ${i} -t 15 -vn -acodec pcm_s16le -ac 2 -ar 48000 file:${RECS_DIR}/$(date "+%F")-birdnet-$(date "+%H:%M:%S").wav </dev/null > /dev/null 2>&1 & sleep 1;
+      ffmpeg -i  ${i} -t 15 -vn -acodec pcm_s16le -ac 2 -ar 48000 file:${RECS_DIR}/$(date "+%F")-birdnet-$(date "+%H:%M:%S").wav
     done
   done
 else
