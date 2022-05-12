b7457624d4b3ae68dcc9c6a99e713b669733549b
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-05-12 16:39:51 -0400

    Update spectrogram.sh

--- scripts/spectrogram.sh
+++ scripts/spectrogram.sh
@@ -3,4 +3,4 @@
 source /etc/birdnet/birdnet.conf
 analyzing_now="$(cat $HOME/BirdNET-Pi/analyzing_now.txt)"
 spectrogram_png=${EXTRACTED}/spectrogram.png
-sox "${analyzing_now}" -n remix 1 rate 24k spectrogram -c "${analyzing_now}" -o "${spectrogram_png}"
+sox "${analyzing_now}" -n remix 1 rate 24k spectrogram -c "${analyzing_now//$HOME\/}" -o "${spectrogram_png}"
