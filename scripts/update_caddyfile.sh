dfa7c8b84e48ea06e4888a342de89355d6bcd8fb
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-05-12 12:24:27 -0400

    Update update_caddyfile.sh

--- scripts/update_caddyfile.sh
+++ scripts/update_caddyfile.sh
@@ -18,6 +18,9 @@ http://localhost http://$(hostname).local ${BIRDNETPI_URL} {
   handle /Charts/* {
     file_server browse
   }
+  basicauth views.php?view=File* {
+    birdnet ${HASHWORD}
+  }
   basicauth /Processed* {
     birdnet ${HASHWORD}
   }
