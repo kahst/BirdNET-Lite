4877d1de66bb7e813062d7047cd45a730535e2e0
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-05-16 10:52:47 -0400

    Revert "cleanup: Localhost everywhere"

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -5,7 +5,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "localhost"
+SERVER = "127.0.1.1"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
