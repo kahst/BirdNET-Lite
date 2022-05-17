b136e07b51c8facc16b41a37487b675c7c9d84d2
Jake Herbst <jmherbst@gmail.com>
2022-05-16 21:32:30 -0400

    Removing the 'sed' added python shebangs

--- scripts/update_birdnet_snippets.sh
+++ scripts/update_birdnet_snippets.sh
@@ -5,13 +5,10 @@ trap 'exit 1' SIGINT SIGHUP
 USER=$(awk -F: '/1000/ {print $1}' /etc/passwd)
 HOME=$(awk -F: '/1000/ {print $6}' /etc/passwd)
 my_dir=$HOME/BirdNET-Pi/scripts
-if ! grep python3 <(head -n1 $my_dir/analyze.py) &>/dev/null;then
-  echo "Ensure all python scripts use the virtual environment"
-  sudo -u$USER sed -si "1 i\\#\!$HOME/BirdNET-Pi/birdnet/bin/python3" $my_dir/*.py
-fi
+
 if ! grep PRIVACY_THRESHOLD /etc/birdnet/birdnet.conf &>/dev/null;then
   sudo -u$USER echo "PRIVACY_THRESHOLD=0" >> /etc/birdnet/birdnet.conf
-  git -C $HOME/BirdNET-Pi rm $my_dir/privacy_server.py 
+  git -C $HOME/BirdNET-Pi rm $my_dir/privacy_server.py
 fi
 if [ -f $my_dir/privacy_server ] || [ -L /usr/local/bin/privacy_server.py ];then
   rm -f $my_dir/privacy_server.py
