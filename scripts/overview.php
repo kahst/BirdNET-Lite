e4a595bacc6b08df887e05e0e4319e17bcf09758
ehpersonal38 <103586016+ehpersonal38@users.noreply.github.com>
2022-05-25 12:05:44 -0400

    No more + signs in attribution titles

--- scripts/overview.php
+++ scripts/overview.php
@@ -222,7 +222,7 @@ body::-webkit-scrollbar {
   }
 
   function setModalText(iter, title, text, authorlink, photolink) {
-    document.getElementById('modalHeading').innerHTML = "Photo: \""+decodeURIComponent(title.replace("+"," "))+"\" Attribution";
+    document.getElementById('modalHeading').innerHTML = "Photo: \""+decodeURIComponent(title.replaceAll("+"," "))+"\" Attribution";
     document.getElementById('modalText').innerHTML = "<div><img style='border-radius:5px' src='"+photolink+"'></div><br><div>Image link: <a target='_blank' href="+text+">"+text+"</a><br>Author link: <a target='_blank' href="+authorlink+">"+authorlink+"</a></div>";
     showDialog();
   }
