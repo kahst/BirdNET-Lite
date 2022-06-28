ca09725e81b096fe6f6f9d60ef09f0c02eca19ba
ehpersonal38 <103586016+ehpersonal38@users.noreply.github.com>
2022-06-28 14:18:05 -0400

    Update weekly_report.sh

--- scripts/weekly_report.sh
+++ scripts/weekly_report.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source /etc/birdnet/birdnet.conf
-echo ${APPRISE_WEEKLY_REPORT}
-
-NOTIFICATION=$(curl 'localhost/views.php?view=Weekly%20Report&ascii=true')
-NOTIFICATION=${NOTIFICATION#*#}
-firstLine=`echo "${NOTIFICATION}" | head -1`
-NOTIFICATION=`echo "${NOTIFICATION}" | tail -n +2`
-$HOME/BirdNET-Pi/birdnet/bin/apprise -vv -t "${firstLine}" -b "${NOTIFICATION}" --config=$HOME/BirdNET-Pi/apprise.txt
+if [ ${APPRISE_WEEKLY_REPORT} == 1 ];then
+	NOTIFICATION=$(curl 'localhost/views.php?view=Weekly%20Report&ascii=true')
+	NOTIFICATION=${NOTIFICATION#*#}
+	firstLine=`echo "${NOTIFICATION}" | head -1`
+	NOTIFICATION=`echo "${NOTIFICATION}" | tail -n +2`
+	$HOME/BirdNET-Pi/birdnet/bin/apprise -vv -t "${firstLine}" -b "${NOTIFICATION}" --config=$HOME/BirdNET-Pi/apprise.txt
+fi
