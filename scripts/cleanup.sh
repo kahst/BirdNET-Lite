b28fc21c5f29b0147698266fc48d74121799507b
mcguirepr89 <mcguirepr89@gmail.com>
2022-07-06 08:14:18 -0400

    typo

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -15,4 +15,4 @@ fi
 
 accumulated_files=$(find $RECS_DIR -path $PROCESSED -prune -o -type f -print | wc -l)
 [ $accumulated_files -ge 10 ] && stop_core_services.sh
-echo "$(date "+%b  %e %I:%M:%S") Stopped Core Services -- It looks like analysis stopped. Check raw recordings in $RECS_DIR and check the birdnet_analysis.service and birdnet_server.service \"journalctl -eu birdnet_analysis -u birdnet_server" | sudo tee -a /var/log/syslog
+echo "$(date "+%b  %e %I:%M:%S") Stopped Core Services -- It looks like analysis stopped. Check raw recordings in $RECS_DIR and check the birdnet_analysis.service and birdnet_server.service \"journalctl -eu birdnet_analysis -u birdnet_server\"" | sudo tee -a /var/log/syslog
