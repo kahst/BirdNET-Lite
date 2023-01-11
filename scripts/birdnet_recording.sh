af7d85b6cbdcfe9e51d2476da34c48c6110eb52d
cgomesu <cf365@cornell.edu>
2023-01-11 13:19:11 -0300

    ffmpeg: disable stdin interaction
    ffmpeg enables interaction on stdin by default, which may have
    undesirable consequences when running ffmpeg in the background.
    adding `-nostdin` disables interaction via the stdin stream.

--- scripts/birdnet_recording.sh
+++ scripts/birdnet_recording.sh
@@ -8,7 +8,7 @@ if [ ! -z $RTSP_STREAM ];then
   [ -d $RECS_DIR/StreamData ] || mkdir -p $RECS_DIR/StreamData
   while true;do
     for i in ${RTSP_STREAM//,/ };do
-      ffmpeg -i  ${i} -t ${RECORDING_LENGTH} -vn -acodec pcm_s16le -ac 2 -ar 48000 file:${RECS_DIR}/StreamData/$(date "+%F")-birdnet-$(date "+%H:%M:%S").wav
+      ffmpeg -nostdin -i  ${i} -t ${RECORDING_LENGTH} -vn -acodec pcm_s16le -ac 2 -ar 48000 file:${RECS_DIR}/StreamData/$(date "+%F")-birdnet-$(date "+%H:%M:%S").wav
     done
   done
 else
