9309e23c3f31dd53089e6b7840156aec3fba9350
ehpersonal38 <103586016+ehpersonal38@users.noreply.github.com>
2023-03-22 13:14:54 -0400

    Change how perms are given for txt files

--- scripts/update_birdnet_snippets.sh
+++ scripts/update_birdnet_snippets.sh
@@ -11,11 +11,7 @@ my_dir=$HOME/BirdNET-Pi/scripts
 #sudo chmod -R g+wr $HOME/*
 find $HOME/* -not -user $USER -execdir sudo -E chown $USER:$USER {} \+
 find $HOME/* -not -user $USER -execdir sudo chmod g+wr {} \+
-files=("$HOME/BirdNET-Pi/apprise.txt" "$HOME/BirdNET-Pi/exclude_species_list.txt" "$HOME/BirdNET-Pi/include_species_list.txt" "$HOME/BirdNET-Pi/scripts/disk_check_exclude.txt" "$HOME/BirdNET-Pi/scripts/blacklisted_images.txt")
-for file in "${files[@]}"
-do
-    sudo chown caddy:caddy "$file"
-done
+chmod 666 ~/BirdNET-Pi/scripts/*.txt
 chmod -R a+w $HOME/BirdNET-Pi/scripts/*.php
 
 # Create blank sitename as it's optional. First time install will use $HOSTNAME.
