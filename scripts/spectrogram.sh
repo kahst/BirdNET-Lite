8b33cee93dc5e92a5b856298f48120677a2a4c89
jaredb7 <sudden760@gmail.com>
2023-04-07 09:46:31 +1000

    Enhancement - Update Spectrogram page to show detection labels on RTSP streams
    Added in logic to handle finding the correct 'newest' detection's CSV file for the stream being listened to, the script will look in the sound card recordings location as usual, if it finds nothing there, then look in the StreamData folder for files matching the currently listened stream
    
    Updated spectrogram.sh to not execute sox if the file mentioned in analyzing now does not exist, this is just to try reduce noise in the logs
    
    Fixed my mis-spelling of RTSP again
    
    Small tweak to the Javascript responsible for drawing the labels on the spectrogram to accommodate the inclusion of RTSP_X in the streams filename

--- scripts/spectrogram.sh
+++ scripts/spectrogram.sh
@@ -2,5 +2,7 @@
 # Make sox spectrogram
 source /etc/birdnet/birdnet.conf
 analyzing_now="$(cat $HOME/BirdNET-Pi/analyzing_now.txt)"
-spectrogram_png=${EXTRACTED}/spectrogram.png
-sox -V1 "${analyzing_now}" -n remix 1 rate 24k spectrogram -c "${analyzing_now//$HOME\/}" -o "${spectrogram_png}"
+if [ ! -z "${analyzing_now}" ] && [ -f "${analyzing_now}" ]; then
+  spectrogram_png=${EXTRACTED}/spectrogram.png
+  sox -V1 "${analyzing_now}" -n remix 1 rate 24k spectrogram -c "${analyzing_now//$HOME\/}" -o "${spectrogram_png}"
+fi
