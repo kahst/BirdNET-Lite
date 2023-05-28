f0be69d79addda6f0297c2808cfd014356e6a6f6
ehpersonal38 <103586016+ehpersonal38@users.noreply.github.com>
2023-05-28 13:40:19 -0400

    Revert "fix species list tester"
    This reverts commit 4b6b75a649d75331ff3d408ffcca0270cea1a3d4.

--- scripts/config.php
+++ scripts/config.php
@@ -113,7 +113,7 @@ if(isset($_GET["latitude"])){
     echo "<script>setTimeout(
     function() {
       const xhttp = new XMLHttpRequest();
-    xhttp.open(\"GET\", \"./views.php?view=Settings&restart_php=true\", true);
+    xhttp.open(\"GET\", \"./config.php?restart_php=true\", true);
     xhttp.send();
     }, 1000);</script>";
   }
@@ -466,7 +466,7 @@ function runProcess() {
       output.innerHTML = xhr.responseText;
     }
   };
-  xhr.open('GET', `views.php?view=Settings&threshold=${threshold}`);
+  xhr.open('GET', `scripts/config.php?threshold=${threshold}`);
   xhr.send();
 }
 </script>
